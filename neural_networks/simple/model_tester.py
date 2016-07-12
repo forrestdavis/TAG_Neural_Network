d0eb556c25ac7a7d465832a229ed6c46eaa68e90
Forrest Davis <fld2111@columbia.edu>
2016-07-12 15:13:18 -0400

    added a initial test to check differences on other machines

--- neural_networks/simple/model_tester.py
+++ neural_networks/simple/model_tester.py
@@ -11,8 +11,8 @@ numpy.random.seed(seed)
 X_test = numpy.load("X_dev.npy")
 Y_test = numpy.load("Y_dev.npy")
 
-model = model_from_json(open('first_model_architecture.json').read())
-model.load_weights('first_model_weights.h5')
+model = model_from_json(open('C_model_architecture.json').read())
+model.load_weights('C_model_weights.h5')
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 #hist = model.fit(X_train, Y_train, nb_epoch=50, batch_size=10)
