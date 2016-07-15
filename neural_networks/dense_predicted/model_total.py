116fdb1510677d29759eb337cf2e18645f3a3105
Forrest Davis <fld2111@columbia.edu>
2016-07-15 14:38:12 -0400

    Added a new perl script from converting from fann to csv file for numpy array in model

--- neural_networks/dense_predicted/model_total.py
+++ neural_networks/dense_predicted/model_total.py
@@ -1,20 +1,31 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 import numpy
+import sys
 
 #Fix random to ensure consistent results during testing
 seed = 4
 numpy.random.seed(seed)
 
-io_info = open("io_dimensions.txt", "r")
-i = io_info.readline().split()
-o = io_info.readline().split()
+io_info_train = open("io_dimensions_train.txt", "r")
+io_info_test = open("io_dimensions_test.txt", "r")
+one = io_info_train.readline().split()
+two = io_info_test.readline().split()
 
-input_dimensions = int(o[1])
-output_dimensions = int(o[2])
+input_dimensions = int(one[0])
+output_dimensions = int(one[1])
+i_dimensions = int(two[0])
+o_dimensions = int(two[1])
 
-X_train = numpy.load("X_train.npy")
-Y_train = numpy.load("Y_train.npy")
+if input_dimensions!= i_dimensions:
+    sys.stderr.write("There is a mismatch in input dimensions\n")
+    sys.exit(1)
+if output_dimensions!= o_dimensions:
+    sys.stderr.write("There is a mismatch in output dimensions\n")
+    sys.exit(1)
+
+X_train = numpy.loadtxt("X_train.csv", delimiter=",")
+Y_train = numpy.loadtxt("Y_train.csv", delimiter=",")
 
 #Create model
 model = Sequential()
@@ -34,8 +45,8 @@ model.compile(loss='categorical_crossentropy',
 hist = model.fit(X_train, Y_train, nb_epoch=50, batch_size=1000)
 
 #Evaluate model
-X_test = numpy.load("X_dev.npy")
-Y_test = numpy.load("Y_dev.npy")
+X_test = numpy.loadtxt("X_test.csv", delimiter=",")
+Y_test = numpy.loadtxt("Y_test.csv", delimiter=",")
 
 scores = model.evaluate(X_test, Y_test)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
