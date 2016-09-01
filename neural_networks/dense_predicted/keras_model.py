555185118bff6f8c772608e2080eb3df3131cd01
Forrest Davis <fld2111@columbia.edu>
2016-09-01 13:18:10 -0400

    Flags are now handled by keras model. main.py is working now

--- neural_networks/dense_predicted/keras_model.py
+++ neural_networks/dense_predicted/keras_model.py
@@ -5,25 +5,40 @@ from keras.utils.visualize_util import plot
 import numpy
 import sys
 import argparse
+import os
 import model_framework as mf 
 
 class KerasModel:
     def __init__(self, data_directory):
+        #Get io file
+        dim_file = ""
+        for filename in os.listdir(data_directory):
+            if "io_" in filename:
+                if data_directory[len(data_directory)-1] == "/":
+                    dim_file = data_directory+filename
+                else:
+                    dim_file = data_directory+"/"+filename
+        if not dim_file:
+            sys.stderr.write("There must be a file with the dimensions "
+                    +"of the data, file name must contain 'io'\n")
+            sys.exit(1)
+
         self.model = Sequential()
-        self.dim_file = data_directory + "/" + "io_dimensions_1000.txt"
+        self.dim_file = dim_file
+        self.data_dir = data_directory
         
-    def train(self):
+    def train(self, find_feats):
         #Train model on data
         #Get dimensions of data
         dimensions_dictionary = mf.get_dimensions(self.dim_file)
 
         #Get train data
-        '''
-        find_feats = ['A', 'B', 'C', 'D', 'E', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'form', 'pos', 'output']
-        train_data, Y_train, feats = mf.getTrainData("data_1000/numpy_arrays", find_feats)
-        '''
-        train_data, Y_train, feats = mf.getTrainData("data_1000/numpy_arrays")
+        if find_feats:
+            train_data, Y_train, feats = mf.getTrainData(
+                    "data_1000/numpy_arrays", find_feats)
+        else:
+            train_data, Y_train, feats = mf.getTrainData(
+                    "data_1000/numpy_arrays")
         
         self.feats = feats
 
@@ -37,16 +52,12 @@ class KerasModel:
             nb_epoch=2, verbose=1, batch_size=1000, validation_split=0.1)
         
     #Need to update predict
-    def predict(self):
+    def predict(self, fann_file, fm_file):
         sys.stderr.write("getting prediction from data...\n")
-
-        fann_file = "../../parser/fv.txt"
-        fm_file = "../../parser/parser.fm"
-        io_file = "data_1000/io_dimensions_1000.txt"
 
         #Get prediction data
         prediction_data, feats = mf.getPredictionData(fann_file, fm_file, 
-                io_file)
+                self.dim)
 
         #need to arrange prediction_data to fit train data form
         prediction_data = mf.arrangeData(prediction_data, self.feats, feats)
@@ -67,22 +78,29 @@ class KerasModel:
         else:
             return "ERROR"
 
-    def save(self):
+    def save(self, filename):
         sys.stderr.write("saving model...\n")
-        saved_directory = "saved_models"
+        saved_directory = "saved_models/"
+        if not filename:
+            filename = "trained_model"
+        saved_file = saved_directory+filename
         model_json = self.model.to_json()
-        with open(saved_directory+"/trained_model.json", "w") as json_file:
+        with open(saved_file+".json", "w") as json_file:
             json_file.write(model_json)
-        self.model.save_weights(saved_directory+"/trained_model_weights.h5", overwrite=True)
-        mf.saveFeats(self.feats, saved_directory+"/trained_model_feats.txt")
+        self.model.save_weights(saved_file+"_weights.h5", 
+                overwrite=True)
+        mf.saveFeats(self.feats, saved_file+"_feats.txt")
 
-    def load(self):
+    def load(self, filename):
         #Load model from saved arch, weights, and features used
         sys.stderr.write("loading model...\n")
-        saved_directory = "saved_models"
-        model_arch = saved_directory+"/trained_model.json"
-        model_weights = saved_directory+"/trained_model_weights.h5"
-        self.feats = mf.loadFeats(saved_directory+"/trained_model_feats.txt")
+        saved_directory = "saved_models/"
+        if not filename:
+            filename="trained_model"
+        saved_file = saved_directory+filename
+        model_arch = saved_file+".json"
+        model_weights = saved_file+"_weights.h5"
+        self.feats = mf.loadFeats(saved_file+"_feats.txt")
         self.model = model_from_json(open(model_arch).read())
         self.model.load_weights(model_weights)
         self.model.compile(loss='categorical_crossentropy', 
@@ -100,82 +118,8 @@ class KerasModel:
         scores = self.model.evaluate(test_data, Y_test)
         print("%s: %.2f%%" % (self.model.metrics_names[1], scores[1]*100))
 
-    def graph(self):
+    def graph(self, filename):
+        if not filename:
+            filename = "graph_model.png"
         sys.stderr.write("graphing model...\n")
-        graph_filename = 'model.png'
-        plot(self.model, graph_filename, show_shapes=True)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage="%(prog)s [options]",
-            add_help=False)
-
-    ############################################################
-    #Flags for options
-    ############################################################
-    req = parser.add_argument_group("Options", 
-            "Arguments -D and -M are required")
-    req.add_argument("-nv", "--no-verbose", 
-            help="deactivate verbose mode", action="store_false")
-    req.add_argument("-h", "--help", action="help",
-            help="display this message")
-    req.add_argument("-D", "--data_path", metavar='', 
-            help="path to data directory", required=True)
-    req.add_argument("-M", "--mode", metavar='', 
-            help="TRAIN|TEST|PREDICT|GRAPH", required=True, 
-            choices=["TRAIN", "TEST", "PREDICT", "GRAPH"])
-    req.add_argument("-T", "--trained_model", metavar='',
-    help=("Specifiy the file name for saving/loading the train model, "
-          +"no extension. The files are "
-          +"located in saved_models "
-          +"{for all modes} (default: trained_model)"))
-    req.add_argument("-L", "--load", action="store_true",
-            help=("Load trained model {for TEST, PREDICT, and GRAPH} "
-    +"(default: False, will train model"))
-
-    ############################################################
-    #Flags for train
-    ############################################################
-    train = parser.add_argument_group("IN TRAIN MODE",
-            "Argument -T, listed above, is an option as well")
-    train.add_argument("-S", "--save", action="store_true", 
-            help="Save trained model (default: False)")
-    train.add_argument("-F", "--feats", metavar='',
-            help=("List features for training (default: all features "
-                +"available in data directory)"))
-
-    ############################################################
-    #Flags for test
-    ############################################################
-    test = parser.add_argument_group("IN TEST MODE",
-            "Optional arguments are -L and -T, listed in Options."
-            +" If a model is not loaded, a model will be trained and "
-            +" the flags for TRAIN mode can also be specified")
-
-    ############################################################
-    #Flags for predict
-    ############################################################
-    predict = parser.add_argument_group("IN PREDICT MODE", 
-            "Argument -L, listed above is an option as well")
-    predict.add_argument("-f", "--fann", metavar='', help="fann file")
-    predict.add_argument("-FM", "--feat_model", metavar='',
-            help="feature model file name")
-
-    ############################################################
-    #Flags for graph
-    ############################################################
-    graph = parser.add_argument_group("IN GRAPH MODE", 
-            "Argument -L, listed above is an option as well."
-            +" If a model is not loaded, a model will be trained and "
-            +" the flags for TRAIN mode can also be specified")
-    graph.add_argument("-g", "--graph", metavar='', help="graph file name")
-
-    args = parser.parse_args()
-
-    #model = KerasModel("data_1000")
-    #model.train()
-    #model.save()
-    #model.load()
-    #model.evaluate()
-    #print model.predict()
-    #model.graph()
+        plot(self.model, filename, show_shapes=True)
--- neural_networks/dense_predicted/main.py
+++ neural_networks/dense_predicted/main.py
@@ -4,7 +4,14 @@ import keras_model as km
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage="%(prog)s [options]",
-            add_help=False)
+            description=("main function for working with a "
+               +"keras model. You can list more than one mode in any order "
+               +"i.e. TEST TRAIN GRAPH. The order of operatations under "
+               +"the hood is TRAIN TEST PREDICT GRAPH. TEST/PREDICT without "
+               +"-L to load a pre-trained model, or adding TRAIN to -M "
+               +"will still cause TRAIN to be called, so TRAIN specific "
+               +"flags can be added to a TEST/PREDICT call")
+                ,add_help=False)
 
     ############################################################
     #Flags for options
@@ -17,7 +24,7 @@ if __name__ == "__main__":
             help="display this message")
     req.add_argument("-D", "--data_path", metavar='', 
             help="path to data directory", required=True)
-    req.add_argument("-M", "--mode", metavar='', 
+    req.add_argument("-M", "--mode", metavar='', nargs='+',
             help="TRAIN|TEST|PREDICT|GRAPH", required=True, 
             choices=["TRAIN", "TEST", "PREDICT", "GRAPH"])
     req.add_argument("-T", "--trained_model", metavar='',
@@ -36,7 +43,7 @@ if __name__ == "__main__":
             "Argument -T, listed above, is an option as well")
     train.add_argument("-S", "--save", action="store_true", 
             help="Save trained model (default: False)")
-    train.add_argument("-F", "--feats", metavar='',
+    train.add_argument("-F", "--feats", metavar='', nargs='+',
             help=("List features for training (default: all features "
                 +"available in data directory)"))
 
@@ -44,7 +51,8 @@ if __name__ == "__main__":
     #Flags for test
     ############################################################
     test = parser.add_argument_group("IN TEST MODE",
-            "Optional arguments are -L and -T, listed in Options."
+            "Optional arguments are -L/--load and -T/--trained_model, "
+            +"listed in Options."
             +" If a model is not loaded, a model will be trained and "
             +" the flags for TRAIN mode can also be specified")
 
@@ -52,7 +60,7 @@ if __name__ == "__main__":
     #Flags for predict
     ############################################################
     predict = parser.add_argument_group("IN PREDICT MODE", 
-            "Argument -L, listed above is an option as well")
+            "Argument -L/--load, listed above is an option as well")
     predict.add_argument("-f", "--fann", metavar='', help="fann file")
     predict.add_argument("-FM", "--feat_model", metavar='',
             help="feature model file name")
@@ -61,17 +69,56 @@ if __name__ == "__main__":
     #Flags for graph
     ############################################################
     graph = parser.add_argument_group("IN GRAPH MODE", 
-            "Argument -L, listed above is an option as well."
+            "Argument -L/--load, listed above is an option as well."
             +" If a model is not loaded, a model will be trained and "
             +" the flags for TRAIN mode can also be specified")
     graph.add_argument("-g", "--graph", metavar='', help="graph file name")
 
     args = parser.parse_args()
 
-    #model = KerasModel("data_1000")
-    #model.train()
-    #model.save()
-    #model.load()
-    #model.evaluate()
-    #print model.predict()
-    #model.graph()
+    model = km.KerasModel(args.data_path)
+    trained=False
+    #TRAIN
+    if "TRAIN" in args.mode:
+        model.train(args.feats)
+        trained=True
+    
+    #SAVE/LOAD
+    if args.trained_model:
+        if not args.save and not args.load:
+            sys.stderr.write("To use -T/--train_model you must either specifiy "
+                    +"-L/--load or -S/--save\n")
+            sys.exit(1)
+    if args.save:
+        model.save(args.trained_model)
+    if args.load:
+        model.load(args.trained_model)
+
+    #TEST
+    if "TEST" in args.mode:
+        if not args.load and not trained:
+            model.train(args.feats)
+            trained=True
+        model.evaluate()
+    
+    #Predict
+    if "PREDICT" in args.mode:
+        if not args.load and not trained:
+            model.train(args.feats)
+            trained=True
+        if not args.feat_model:
+            sys.stderr.write(
+                    "To PREDICT -FM/--feat_model must be specified\n")
+            sys.exit(1)
+        if not args.fann:
+            sys.stderr.write(
+                    "To PREDICT -f/--fann must be specified\n")
+            sys.exit(1)
+        print model.predict(args.feat_model, args.fann)
+    
+    #Graph
+    if "GRAPH" in args.mode:
+        if not args.load and not trained:
+            model.train(args.feats)
+            trained=True
+        model.graph(args.graph)
