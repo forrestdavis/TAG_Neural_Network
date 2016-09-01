9ae729dfd6fb42dbb5a12cc425f67847bc498e7e
Forrest Davis <fld2111@columbia.edu>
2016-09-01 01:04:53 -0400

    Command line flags help screen has been added to keras model. Now just have to change functions to handle flags.

--- neural_networks/dense_predicted/keras_model.py
+++ neural_networks/dense_predicted/keras_model.py
@@ -4,6 +4,7 @@ from keras.callbacks import EarlyStopping
 from keras.utils.visualize_util import plot
 import numpy
 import sys
+import argparse
 import model_framework as mf 
 
 class KerasModel:
@@ -91,11 +92,6 @@ class KerasModel:
         #Evaluate test data on model
         sys.stderr.write("evaluating model on test data...\n")
         #Get test data
-        '''
-        find_feats = ['A', 'B', 'C', 'D', 'E', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'form', 'pos', 'output']
-        test_data, Y_test, feats = d.getTestData("data_1000/numpy_arrays", find_feats)
-        '''
         test_data, Y_test, feats = mf.getTestData("data_1000/numpy_arrays")
 
         #need to arrange test_data to fit train data form
@@ -111,10 +107,74 @@ class KerasModel:
 
 
 if __name__ == "__main__":
-    model = KerasModel("data_1000")
-    model.train()
+    parser = argparse.ArgumentParser(usage="%(prog)s [options]",
+            add_help=False)
+
+    ############################################################
+    #Flags for options
+    ############################################################
+    req = parser.add_argument_group("Options", 
+            "Arguments -D and -M are required")
+    req.add_argument("-nv", "--no-verbose", 
+            help="deactivate verbose mode", action="store_false")
+    req.add_argument("-h", "--help", action="help",
+            help="display this message")
+    req.add_argument("-D", "--data_path", metavar='', 
+            help="path to data directory")
+    req.add_argument("-M", "--mode", metavar='', 
+            help="TRAIN|TEST|PREDICT|GRAPH")
+    req.add_argument("-T", "--trained_model", metavar='',
+    help=("Specifiy the file name for saving/loading the train model, "
+          +"no extension. The files are "
+          +"located in saved_models "
+          +"{for all modes} (default: trained_model)"))
+    req.add_argument("-L", "--load", action="store_true",
+            help=("Load trained model {for TEST, PREDICT, and GRAPH} "
+    +"(default: False, will train model"))
+
+    ############################################################
+    #Flags for train
+    ############################################################
+    train = parser.add_argument_group("IN TRAIN MODE",
+            "Argument -T, listed above, is an option as well")
+    train.add_argument("-S", "--save", action="store_true", 
+            help="Save trained model (default: False)")
+    train.add_argument("-F", "--feats", metavar='',
+            help=("List features for training (default: all features "
+                +"available in data directory)"))
+
+    ############################################################
+    #Flags for test
+    ############################################################
+    test = parser.add_argument_group("IN TEST MODE",
+            "Optional arguments are -L and -T, listed in Options."
+            +" If a model is not loaded, a model will be trained and "
+            +" the flags for TRAIN mode can also be specified")
+
+    ############################################################
+    #Flags for predict
+    ############################################################
+    predict = parser.add_argument_group("IN PREDICT MODE", 
+            "Argument -L, listed above is an option as well")
+    predict.add_argument("-f", "--fann", metavar='', help="fann file")
+    predict.add_argument("-FM", "--feat_model", metavar='',
+            help="feature model file name")
+
+    ############################################################
+    #Flags for graph
+    ############################################################
+    graph = parser.add_argument_group("IN GRAPH MODE", 
+            "Argument -L, listed above is an option as well."
+            +" If a model is not loaded, a model will be trained and "
+            +" the flags for TRAIN mode can also be specified")
+    graph.add_argument("-g", "--graph", metavar='', help="graph file name")
+
+    args = parser.parse_args()
+
+    #model = KerasModel("data_1000")
+    #model.train()
     #model.save()
     #model.load()
     #model.evaluate()
     #print model.predict()
-    model.graph()
+    #model.graph()
