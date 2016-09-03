c2a2b7ebb9bb9c992c5ea4025b2835d06660f73a
Forrest Davis <fld2111@columbia.edu>
2016-09-02 21:30:25 -0400

    Removed obsolete data folders

--- data/dense_gold_fann/dev_fann.sh
+++
@@ -1,1 +0,0 @@
-maca_trans_parser_conll2fann -o TEST -i ../d6.dev.conll16 -y gold_dev.fann -V test.alpha -F new.fm -C new.mcd
--- data/simple_fann/test_fann.sh
+++
@@ -1,1 +0,0 @@
-maca_trans_parser_conll2fann -o TRAIN -i ../d6.train.conll16 -s 1000 -y train.fann -V test.alpha -F new.fm -C new.mcd
